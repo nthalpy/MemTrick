c3ca57b827b4b8117ef5cdf203801c951bd37945
Harnel <dcrgkev@gmail.com>
2019-09-30 20:56:56 +0900

    Tests and some implementation related comments added.

--- MemTrick.CLR.Test.Shared/MemoryRestrictorTest.cs
+++ MemTrick.CLR.Test.Shared/MemoryRestrictorTest.cs
@@ -2,6 +2,8 @@
 using MemTrick.CLR.Test.Infra;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace MemTrick.CLR.Test
 {
@@ -22,14 +24,48 @@ namespace MemTrick.CLR.Test
 
         [TestMethod]
         [ExpectedException(typeof(MemoryRestrictorException))]
-        public void TestAllocation()
+        public void ThrowOnAllocation()
         {
             using (NoAllocFinalizer _ = MemoryRestrictor.StartNoAlloc())
             {
                 Object obj = new Object();
 
-                // Need to use obj variable to prevent being optimized out.
+                // Need to use 'obj' variable to prevent being optimized out.
                 Assert.IsNotNull(obj);
+            }
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(MemoryRestrictorException))]
+        public void ThrowOnLinq()
+        {
+            int[] array = new int[] { 1, 2, 3, 4 };
+            int sum = 0;
+
+            using (NoAllocFinalizer _ = MemoryRestrictor.StartNoAlloc())
+            {
+                // Enumerable.Sum(this IEnumerable`1) extension method internally uses 
+                // IEnumerable`1.GetEnumerator, and invoking IEnumerable`1.GetEnumerator
+                // on array makes allocation. (See SZArrayHelper.GetEnumerator method)
+                sum = array.Sum();
+            }
+
+            Assert.AreEqual(sum, 10);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(MemoryRestrictorException))]
+        // TODO: Make this test pass by hijacking method which assigned to
+        // hlpDynamicFuncTable[CORINFO_HELP_BOX].
+        public void ThrowOnBoxing()
+        {
+            using (NoAllocFinalizer _ = MemoryRestrictor.StartNoAlloc())
+            {
+                int intValue = 0x123123;
+
+                // Invoking Object.GetHashCode will box 'intValue' variable first,
+                // Then invoke it with.
+                Assert.IsTrue(intValue.GetHashCode() == intValue);
             }
         }
     }
