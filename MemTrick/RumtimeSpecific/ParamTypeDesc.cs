544dfd8de9d67a849327c19fbbaf32f57fc3aad0
Harnel <dcrgkev@gmail.com>
2020-01-06 00:07:57 +0900

    Added temporary code to fix ParamTypeDesc.GetTemplateMethodTableInternal

--- MemTrick/RumtimeSpecific/ParamTypeDesc.cs
+++ MemTrick/RumtimeSpecific/ParamTypeDesc.cs
@@ -8,14 +8,23 @@ namespace MemTrick.RumtimeSpecific
     {
         public TypeDesc Parent;
 
-        private UInt64 templateMT;
+        private IntPtr templateMT;
         public TypeHandle Arg;
         private IntPtr exposedClassObject;
 
         internal MethodTable* GetTemplateMethodTableInternal()
         {
-            fixed (ParamTypeDesc* pThis = &this)
-                return (MethodTable*)((Byte*)&pThis->templateMT + templateMT);
+            // TODO: Separate this logic
+            if ((UInt64)templateMT > 0x1000000)
+            {
+                // .NET Framework 4.5~4.8, .NET Core 2.0 ~ 2.0.9 uses this logic:
+                return (MethodTable*)templateMT;
+            }
+            else
+            {
+                fixed (ParamTypeDesc* pThis = &this)
+                    return (MethodTable*)((Byte*)&pThis->templateMT + (UInt64)templateMT);
+            }
         }
     }
 }
