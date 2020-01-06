f8fd8101ed8de1ea9a1aa7c4d8e2726494609bfc
Harnel <dcrgkev@gmail.com>
2020-01-06 23:25:51 +0900

    Add more Allocate<T, ...> overrides

--- MemTrick/RumtimeSpecific/ArbitaryMethodInvoker.cs
+++ MemTrick/RumtimeSpecific/ArbitaryMethodInvoker.cs
@@ -2,68 +2,119 @@
 
 namespace MemTrick.RumtimeSpecific
 {
-    internal static class ArbitaryMethodInvoker
+    internal static unsafe class ArbitaryMethodInvoker
     {
+        private static UnmanagedHeapDisposeHandle CreateAction<TAction, TArg0>(TArg0 arg0, IntPtr pMethod, out TAction action)
+            where TAction : Delegate
+            where TArg0 : class
+        {
+            MethodTable* pMT = MethodTable.GetMethodTable<TAction>();
+
+            ObjectHeader* pThis = TypedReferenceHelper.ClassToPointer(arg0);
+            ObjectHeader* objHeader = (ObjectHeader*)RawMemoryAllocator.Allocate(pMT->BaseSize);
+
+            try
+            {
+                objHeader->SyncBlock = 0;
+                objHeader->MethodTable = pMT;
+
+                Byte* objBody = (Byte*)(objHeader + 1);
+                *(MethodTable***)objBody = &pThis->MethodTable; // Delegate._target
+                *(IntPtr*)(objBody + 2 * sizeof(IntPtr)) = pMethod; // Delegate._methodPtr
+
+                action = TypedReferenceHelper.PointerToObject<TAction>(objHeader);
+            }
+            catch
+            {
+                RawMemoryAllocator.Free(objHeader);
+                throw;
+            }
+
+            return new UnmanagedHeapDisposeHandle(objHeader);
+        }
+
         /// <summary>
         /// Invoke pMethod(arg0), or arg0.pMethod()
         /// </summary>
-        public static void InvokeAction<T0>(IntPtr pMethod, T0 arg0) where T0 : class
+        public static void InvokeAction<T0>(
+            IntPtr pMethod,
+            T0 arg0)
+            where T0 : class
         {
-            unsafe
-            {
-                MethodTable* pMT = MethodTable.GetMethodTable<Action>();
-
-                ObjectHeader* pThis = TypedReferenceHelper.ClassToPointer(arg0);
-                ObjectHeader* objHeader = (ObjectHeader*)RawMemoryAllocator.Allocate(pMT->BaseSize);
-
-                try
-                {
-                    objHeader->SyncBlock = 0;
-                    objHeader->MethodTable = pMT;
-
-                    Byte* objBody = (Byte*)(objHeader + 1);
-                    *(MethodTable***)objBody = &pThis->MethodTable; // Delegate._target
-                    *(IntPtr*)(objBody + 2 * sizeof(IntPtr)) = pMethod; // Delegate._methodPtr
-
-                    Action action = TypedReferenceHelper.PointerToObject<Action>(objHeader);
-                    action.Invoke();
-                }
-                finally
-                {
-                    RawMemoryAllocator.Free(objHeader);
-                }
-            }
+            using (CreateAction(arg0, pMethod, out Action action))
+                action.Invoke();
         }
 
         /// <summary>
         /// Invoke pMethod(arg0, arg1), or arg0.pMethod(arg1)
         /// </summary>
-        public static void InvokeAction<T0, T1>(IntPtr pMethod, T0 arg0, T1 arg1) where T0 : class
+        public static void InvokeAction<T0, T1>(
+            IntPtr pMethod,
+            T0 arg0, T1 arg1)
+            where T0 : class
         {
-            unsafe
-            {
-                MethodTable* pMT = MethodTable.GetMethodTable<Action<T1>>();
+            using (CreateAction(arg0, pMethod, out Action<T1> action))
+                action.Invoke(arg1);
+        }
 
-                ObjectHeader* pThis = TypedReferenceHelper.ClassToPointer(arg0);
-                ObjectHeader* objHeader = (ObjectHeader*)RawMemoryAllocator.Allocate(pMT->BaseSize);
+        /// <summary>
+        /// Invoke pMethod(arg0, arg1, arg2), or arg0.pMethod(arg1, arg2)
+        /// </summary>
+        public static void InvokeAction<T0, T1, T2>(
+            IntPtr pMethod,
+            T0 arg0, T1 arg1, T2 arg2)
+            where T0 : class
+        {
+            using (CreateAction(arg0, pMethod, out Action<T1, T2> action))
+                action.Invoke(arg1, arg2);
+        }
 
-                try
-                {
-                    objHeader->SyncBlock = 0;
-                    objHeader->MethodTable = pMT;
+        /// <summary>
+        /// Invoke pMethod(arg0, arg1, arg2, arg3), or arg0.pMethod(arg1, arg2, arg3)
+        /// </summary>
+        public static void InvokeAction<T0, T1, T2, T3>(
+            IntPtr pMethod,
+            T0 arg0, T1 arg1, T2 arg2, T3 arg3)
+            where T0 : class
+        {
+            using (CreateAction(arg0, pMethod, out Action<T1, T2, T3> action))
+                action.Invoke(arg1, arg2, arg3);
+        }
 
-                    Byte* objBody = (Byte*)(objHeader + 1);
-                    *(MethodTable***)objBody = &pThis->MethodTable; // Delegate._target
-                    *(IntPtr*)(objBody + 2 * sizeof(IntPtr)) = pMethod; // Delegate._methodPtr
+        /// <summary>
+        /// Invoke pMethod(arg0, arg1, arg2, arg3, arg4), or arg0.pMethod(arg1, arg2, arg3, arg4)
+        /// </summary>
+        public static void InvokeAction<T0, T1, T2, T3, T4>(
+            IntPtr pMethod,
+            T0 arg0, T1 arg1, T2 arg2, T3 arg3, T4 arg4)
+            where T0 : class
+        {
+            using (CreateAction(arg0, pMethod, out Action<T1, T2, T3, T4> action))
+                action.Invoke(arg1, arg2, arg3, arg4);
+        }
 
-                    Action<T1> action = TypedReferenceHelper.PointerToObject<Action<T1>>(objHeader);
-                    action.Invoke(arg1);
-                }
-                finally
-                {
-                    RawMemoryAllocator.Free(objHeader);
-                }
-            }
+        /// <summary>
+        /// Invoke pMethod(arg0, arg1, arg2, arg3, arg4, arg5), or arg0.pMethod(arg1, arg2, arg3, arg4, arg5)
+        /// </summary>
+        public static void InvokeAction<T0, T1, T2, T3, T4, T5>(
+            IntPtr pMethod,
+            T0 arg0, T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5)
+            where T0 : class
+        {
+            using (CreateAction(arg0, pMethod, out Action<T1, T2, T3, T4, T5> action))
+                action.Invoke(arg1, arg2, arg3, arg4, arg5);
+        }
+
+        /// <summary>
+        /// Invoke pMethod(arg0, arg1, arg2, arg3, arg4, arg5, arg6), or arg0.pMethod(arg1, arg2, arg3, arg4, arg5, arg6)
+        /// </summary>
+        public static void InvokeAction<T0, T1, T2, T3, T4, T5, T6>(
+            IntPtr pMethod,
+            T0 arg0, T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5, T6 arg6)
+            where T0 : class
+        {
+            using (CreateAction(arg0, pMethod, out Action<T1, T2, T3, T4, T5, T6> action))
+                action.Invoke(arg1, arg2, arg3, arg4, arg5, arg6);
         }
     }
 }
--- MemTrick/UnmanagedHeapAllocator.cs
+++ MemTrick/UnmanagedHeapAllocator.cs
@@ -4,7 +4,7 @@ using System.Reflection;
 
 namespace MemTrick
 {
-    public static class UnmanagedHeapAllocator
+    public static unsafe class UnmanagedHeapAllocator
     {
         /// <summary>
         /// Similar with FormatterServices.GetUninitializedObject. Allocate and return zeroed T-typed object.
@@ -17,27 +17,35 @@ namespace MemTrick
             return handle;
         }
 
+        private static ConstructorInfo GetConstructorInfo<T>(params Type[] types)
+        {
+            return typeof(T).GetConstructor(
+                BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.CreateInstance,
+                null,
+                CallingConventions.Any,
+                types,
+                null);
+        }
+
         /// <summary>
         /// Similar with FormatterServices.GetUninitializedObject. Allocate and return zeroed T-typed object.
         /// </summary>
         public static UnmanagedHeapDisposeHandle UninitializedAllocation(Type t, out Object result)
         {
-            unsafe
-            {
-                MethodTable* mt = MethodTable.GetMethodTable(t);
-                int size = mt->BaseSize;
+            MethodTable* mt = MethodTable.GetMethodTable(t);
+            int size = mt->BaseSize;
 
-                ObjectHeader* objHeader = (ObjectHeader*)RawMemoryAllocator.Allocate(size);
+            ObjectHeader* objHeader = (ObjectHeader*)RawMemoryAllocator.Allocate(size);
 
-                objHeader->SyncBlock = 0;
-                objHeader->MethodTable = mt;
-                RawMemoryAllocator.FillMemory(objHeader + 1, 0, size - sizeof(ObjectHeader));
+            objHeader->SyncBlock = 0;
+            objHeader->MethodTable = mt;
+            RawMemoryAllocator.FillMemory(objHeader + 1, 0, size - sizeof(ObjectHeader));
 
-                result = TypedReferenceHelper.PointerToObject<Object>(objHeader);
-                return new UnmanagedHeapDisposeHandle(objHeader);
-            }
+            result = TypedReferenceHelper.PointerToObject<Object>(objHeader);
+            return new UnmanagedHeapDisposeHandle(objHeader);
         }
 
+        #region Allocate<T, ...> series
         /// <summary>
         /// Similar with new T();
         /// </summary>
@@ -45,16 +53,8 @@ namespace MemTrick
         {
             UnmanagedHeapDisposeHandle handle = UninitializedAllocation<T>(out result);
 
-            unsafe
-            {
-                ConstructorInfo ci = typeof(T).GetConstructor(
-                    BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.CreateInstance,
-                    null,
-                    CallingConventions.Any,
-                    Type.EmptyTypes,
-                    null);
-                ArbitaryMethodInvoker.InvokeAction(ci.MethodHandle.GetFunctionPointer(), result);
-            }
+            ConstructorInfo ci = GetConstructorInfo<T>(Type.EmptyTypes);
+            ArbitaryMethodInvoker.InvokeAction(ci.MethodHandle.GetFunctionPointer(), result);
 
             return handle;
         }
@@ -62,42 +62,115 @@ namespace MemTrick
         /// <summary>
         /// Similar with new T(arg0);
         /// </summary>
-        public static UnmanagedHeapDisposeHandle Allocate<T, TArg0>(out T result, TArg0 arg0) where T : class
+        public static UnmanagedHeapDisposeHandle Allocate<T, TArg0>(
+            out T result, 
+            TArg0 arg0)
+            where T : class
         {
             UnmanagedHeapDisposeHandle handle = UninitializedAllocation<T>(out result);
 
-            unsafe
-            {
-                ConstructorInfo ci = typeof(T).GetConstructor(
-                    BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.CreateInstance,
-                    null,
-                    CallingConventions.Any,
-                    new Type[] { typeof(TArg0) },
-                    null);
-                ArbitaryMethodInvoker.InvokeAction(ci.MethodHandle.GetFunctionPointer(), result, arg0);
-            }
+            ConstructorInfo ci = GetConstructorInfo<T>(typeof(TArg0));
+            ArbitaryMethodInvoker.InvokeAction(ci.MethodHandle.GetFunctionPointer(), result, arg0);
 
             return handle;
         }
 
+        /// <summary>
+        /// Similar with new T(arg0, arg1);
+        /// </summary>
+        public static UnmanagedHeapDisposeHandle Allocate<T, TArg0, TArg1>(
+            out T result,
+            TArg0 arg0, TArg1 arg1)
+            where T : class
+        {
+            UnmanagedHeapDisposeHandle handle = UninitializedAllocation<T>(out result);
+
+            ConstructorInfo ci = GetConstructorInfo<T>(typeof(TArg0), typeof(TArg1));
+            ArbitaryMethodInvoker.InvokeAction(ci.MethodHandle.GetFunctionPointer(), result, arg0, arg1);
+
+            return handle;
+        }
+
+        /// <summary>
+        /// Similar with new T(arg0, arg1, arg2);
+        /// </summary>
+        public static UnmanagedHeapDisposeHandle Allocate<T, TArg0, TArg1, TArg2>(
+            out T result,
+            TArg0 arg0, TArg1 arg1, TArg2 arg2)
+            where T : class
+        {
+            UnmanagedHeapDisposeHandle handle = UninitializedAllocation<T>(out result);
+
+            ConstructorInfo ci = GetConstructorInfo<T>(typeof(TArg0), typeof(TArg1), typeof(TArg2));
+            ArbitaryMethodInvoker.InvokeAction(ci.MethodHandle.GetFunctionPointer(), result, arg0, arg1, arg2);
+
+            return handle;
+        }
+
+        /// <summary>
+        /// Similar with new T(arg0, arg1, arg2, arg3);
+        /// </summary>
+        public static UnmanagedHeapDisposeHandle Allocate<T, TArg0, TArg1, TArg2, TArg3>(
+            out T result,
+            TArg0 arg0, TArg1 arg1, TArg2 arg2, TArg3 arg3)
+            where T : class
+        {
+            UnmanagedHeapDisposeHandle handle = UninitializedAllocation<T>(out result);
+
+            ConstructorInfo ci = GetConstructorInfo<T>(typeof(TArg0), typeof(TArg1), typeof(TArg2), typeof(TArg3));
+            ArbitaryMethodInvoker.InvokeAction(ci.MethodHandle.GetFunctionPointer(), result, arg0, arg1, arg2, arg3);
+
+            return handle;
+        }
+
+        /// <summary>
+        /// Similar with new T(arg0, arg1, arg2, arg3, arg4);
+        /// </summary>
+        public static UnmanagedHeapDisposeHandle Allocate<T, TArg0, TArg1, TArg2, TArg3, TArg4>(
+            out T result,
+            TArg0 arg0, TArg1 arg1, TArg2 arg2, TArg3 arg3, TArg4 arg4)
+            where T : class
+        {
+            UnmanagedHeapDisposeHandle handle = UninitializedAllocation<T>(out result);
+
+            ConstructorInfo ci = GetConstructorInfo<T>(typeof(TArg0), typeof(TArg1), typeof(TArg2), typeof(TArg3), typeof(TArg4));
+            ArbitaryMethodInvoker.InvokeAction(ci.MethodHandle.GetFunctionPointer(), result, arg0, arg1, arg2, arg3, arg4);
+
+            return handle;
+        }
+
+        /// <summary>
+        /// Similar with new T(arg0, arg1, arg2, arg3, arg4, arg5);
+        /// </summary>
+        public static UnmanagedHeapDisposeHandle Allocate<T, TArg0, TArg1, TArg2, TArg3, TArg4, TArg5>(
+            out T result,
+            TArg0 arg0, TArg1 arg1, TArg2 arg2, TArg3 arg3, TArg4 arg4, TArg5 arg5)
+            where T : class
+        {
+            UnmanagedHeapDisposeHandle handle = UninitializedAllocation<T>(out result);
+
+            ConstructorInfo ci = GetConstructorInfo<T>(typeof(TArg0), typeof(TArg1), typeof(TArg2), typeof(TArg3), typeof(TArg4), typeof(TArg5));
+            ArbitaryMethodInvoker.InvokeAction(ci.MethodHandle.GetFunctionPointer(), result, arg0, arg1, arg2, arg3, arg4, arg5);
+
+            return handle;
+        }
+        #endregion
+
         public static UnmanagedHeapDisposeHandle Box<T>(T val, out Object boxed) where T : struct
         {
-            unsafe
-            {
-                MethodTable* mt = MethodTable.GetMethodTable<T>();
-                int size = mt->BaseSize;
+            MethodTable* mt = MethodTable.GetMethodTable<T>();
+            int size = mt->BaseSize;
 
-                ObjectHeader* objHeader = (ObjectHeader*)RawMemoryAllocator.Allocate(size);
+            ObjectHeader* objHeader = (ObjectHeader*)RawMemoryAllocator.Allocate(size);
 
-                void* src = TypedReferenceHelper.StructToPointer(ref val);
+            void* src = TypedReferenceHelper.StructToPointer(ref val);
 
-                objHeader->SyncBlock = 0;
-                objHeader->MethodTable = mt;
-                RawMemoryAllocator.MemCpy(objHeader + 1, src, mt->DataSize);
+            objHeader->SyncBlock = 0;
+            objHeader->MethodTable = mt;
+            RawMemoryAllocator.MemCpy(objHeader + 1, src, mt->DataSize);
 
-                boxed = TypedReferenceHelper.PointerToObject<Object>(objHeader);
-                return new UnmanagedHeapDisposeHandle(objHeader);
-            }
+            boxed = TypedReferenceHelper.PointerToObject<Object>(objHeader);
+            return new UnmanagedHeapDisposeHandle(objHeader);
         }
     }
 }
