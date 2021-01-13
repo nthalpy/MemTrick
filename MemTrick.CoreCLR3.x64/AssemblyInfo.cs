c0ec1d767765a29f7c7e0a1c2ef634eab597d34b
Harnel <dcrgkev@gmail.com>
2021-01-13 15:01:27 +0900

    Add .NETCore 3 target and .NETCore 5 target.

---
+++ MemTrick.CoreCLR3.x64/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+using System.Runtime.CompilerServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+// [assembly: AssemblyTitle("MemTrick.CoreCLR2.x64")]
+// [assembly: AssemblyDescription("")]
+// [assembly: AssemblyConfiguration("")]
+// [assembly: AssemblyCompany("")]
+// [assembly: AssemblyProduct("MemTrick.CoreCLR2.x64")]
+// [assembly: AssemblyCopyright("Copyright ©  2019")]
+// [assembly: AssemblyTrademark("")]
+// [assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+// [assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+// [assembly: Guid("5ec50f49-8402-4621-830f-ccdc45e212bb")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// [assembly: AssemblyVersion("1.0.0.0")]
+// [assembly: AssemblyFileVersion("1.0.0.0")]
+
+[assembly: InternalsVisibleTo("MemTrick.Test.CoreCLR3.x64")]
---
+++ MemTrick.CoreCLR5.x64/RuntimeSpecific.cs
@@ -0,0 +1,7 @@
+﻿namespace MemTrick
+{
+    internal static class RuntimeSpecific
+    {
+        public const int DacOffset = 0xB0;
+    }
+}
